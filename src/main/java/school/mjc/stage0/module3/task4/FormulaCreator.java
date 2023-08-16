6dc3482413ae1e30d10b262fe9cf55027e6fd1b9
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-15 23:00:18 -0600

    All variables declared

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+    char a = 'a';
+    char b = 'b';
+    byte nine = 9;
+    byte five = 5;
+    byte two = 2;
+    byte seven = 7;
+    byte four = 4;
+    int formula = (nine*a^two-five*a+two+a-seven)*(a+b-four*a+b)/two;
     }
 }
