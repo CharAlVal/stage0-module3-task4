4c31a20d232daed98e7053ad2bb8e642b5378716
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-15 23:45:34 -0600

    Corrected prints and formula

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,11 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
     char a = 'a';
     char b = 'b';
-    byte nine = 9;
-    byte five = 5;
-    byte two = 2;
-    byte seven = 7;
-    byte four = 4;
-    int formula = (nine*a^two-five*a+two+a-seven)*(a+b-four*a+b)/two;
+
+    int formula = (9*a^2-5*a+2+a-7)*((a+b-4*a+b)/2);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,10 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c = 99;
-        char h = 104;
-        char a = 97;
-        char r = 114;
-        System.out.println(c+","+h+","+a+","+r);
+        char a = 99;
+        char b = 104;
+        char c = 97;
+        char d = 114;
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
     }
 }
